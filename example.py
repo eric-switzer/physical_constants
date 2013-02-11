81284eee5e34e52158e690ceeb0ddbdf0b782b89
Eric Switzer <eric.switzer@gmail.com>
2013-02-10 20:44:19 -0500

    added astrophysical

--- example.py
+++ example.py
@@ -5,10 +5,12 @@ const_lib = physical_constants.PhysicalConstants()
 
 #const_lib.load(param_root + "NIST_constants_Feb2013_cgs.json")
 const_lib.load("NIST_constants_Feb2013_cgs.json")
+const_lib.load("astrophysical.json")
 const_lib.add_const("one", 1.)
 const_lib.save("combined_constants.json")
 
 const_dict = const_lib.emit()
 
 print const_dict['Newtonian_constant_of_gravitation']
+print const_dict['critical_density_g_cm']
 print const_dict["one"]
